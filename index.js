0f5deafcdff067392f13b50f0bc09dbb91de3019
lbcanfield <lewie.b.canfield@gmail.com>
2023-04-06 14:25:33 -0500

    Instructor class completed.

--- index.js
+++ index.js
@@ -144,8 +144,19 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
-
+class Instructor extends Lambdasian {
+     constructor({ name, age, location, specialty, favLanguage, catchPhrase }) {
+          super({ name, age, location, specialty, favLanguage, catchPhrase });
+          this.specialty = specialty;
+          this.favLanguage = favLanguage;
+          this.catchPhrase = catchPhrase;
+     }
+     demo(subject) {
+          return `Today we are learningn about ${subject}`
+     }
+     grade(studentObj, subject) {
+          return `${studentObj.name} receives a perfect score on ${subject}`
+     }
 }
 
 /*
